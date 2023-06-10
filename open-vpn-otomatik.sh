6d6790766e658c24b02539e192d2600b62cb5f1f
Emre Çağlar YILDIZ <emrecaglar2@aof.anadolu.edu.tr>
2023-06-10 22:10:44 +0300

    Update open-vpn-otomatik.sh
    Otomatik olarak openvpn kurmak için kullanabilirisniz. 
    You can use it to automatically install openvpn.

--- open-vpn-otomatik.sh
+++ open-vpn-otomatik.sh
@@ -2,7 +2,7 @@
 #
 # https://github.com/Nyr/openvpn-install
 #
-# Copyright (c) 2013 Nyr. Released under the MIT License.
+# Copyright (c) 2013 Nyr. Released under the MIT License. 
 
 
 # Detect Debian users running the script with "sh" instead of bash
